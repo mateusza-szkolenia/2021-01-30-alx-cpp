1126f475bf0566252b1f9d7832083f0e75893446
Krzysztof Adamek <k.adamek@alx.pl>
2021-01-30 15:22:58 +0100

    import pliku 14_petla_while.cpp

---
+++ dzien01/14_petla_while.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x = 0;
+    while (x < 10) {
+        std::cout << x << " ble\n";
+        x += 1;
+    }
+    std::cout << "koniec\n";
+    return 0;
+}
