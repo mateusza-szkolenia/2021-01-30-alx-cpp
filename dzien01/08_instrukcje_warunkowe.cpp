fdc5c2b590d9dde8444caebc51367de88607ec0f
Krzysztof Adamek <k.adamek@alx.pl>
2021-01-30 12:51:55 +0100

    import pliku 08_instrukcje_warunkowe.cpp

---
+++ dzien01/08_instrukcje_warunkowe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a = 55;
+    if (a < 10) {
+        std::cout << "lalala\n";
+    }
+    else {
+        std::cout << "ffffffffffffffffffff\n";
+    }
+
+    return 0;
+}
