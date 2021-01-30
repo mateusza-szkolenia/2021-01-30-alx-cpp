b418c90bffcafa820d2dde0d24c921099754e18d
Krzysztof Adamek <k.adamek@alx.pl>
2021-01-30 12:24:22 +0100

    import pliku 07_operatory_logiczne.cpp

---
+++ dzien01/07_operatory_logiczne.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    bool prawda = true;
+    bool falsz = false;
+    std::cout << prawda << '\n';
+    std::cout << falsz << '\n';
+
+    // < - mniejsze
+    // <= - mniejsze lub równe
+    // > - większe
+    // >= - większe lub równe
+    // == - równe
+    // != - różne
+    bool b = 3 == 3;
+    std::cout << b << '\n';
+
+    return 0;
+}
