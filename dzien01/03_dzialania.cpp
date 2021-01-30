d4bccf8194166ca1b1a356573c2f17301299ab04
Krzysztof Adamek <k.adamek@alx.pl>
2021-01-30 10:43:10 +0100

    import pliku 03_dzialania.cpp

---
+++ dzien01/03_dzialania.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//wypisz cyfrę jedności z liczby zapisanej w zmiennej
+
+int main() {
+    int a = 7, b = 3;
+    std::cout << a << '\n';
+    std::cout << b << '\n';
+    std::cout << a + b << '\n';
+    std::cout << a - b << '\n';
+    std::cout << a * b << '\n';
+    std::cout << a / b << '\n';
+    std::cout << a % b << '\n';
+    return 0;
+}
