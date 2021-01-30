8186d9e8337969b26e6307270e1fa17edf813fb8
Krzysztof Adamek <k.adamek@alx.pl>
2021-01-30 14:56:34 +0100

    import pliku 13_trojkaty.cpp

---
+++ dzien01/13_trojkaty.cpp
@@ -0,0 +1,15 @@
+// pobieramy 3 liczby i sprawdzamy czy z odcinków takiej długości da się zbudować trójkąt
+
+#include <iostream>
+
+int main() {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    if (a + b > c && a + c > b && b + c > a) {
+        std::cout << "OK\n";
+    }
+    else {
+        std::cout << "Nie da się :<\n";
+    }
+    return 0;
+}
