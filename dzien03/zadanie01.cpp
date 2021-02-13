a5f60780c8f521b23b1d1553b7d3ac38200be8e3
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-13 11:57:43 +0100

    średnia

--- dzien03/zadanie01.cpp
+++ dzien03/zadanie01.cpp
@@ -12,6 +12,7 @@ int main()
     int liczby[ILE];
 
     int suma = 0;
+    double srednia;
 
     for(int i = 0; i < ILE; i += 1){
         std::cin >> liczby[i];
@@ -21,7 +22,10 @@ int main()
         suma += liczby[i];
     }
 
+    srednia = 1.0 * suma / ILE;
+
     std::cout << "Suma: " << suma << "\n";
+    std::cout << "Średnia: " << srednia << "\n";
 
     return 0;
 }
