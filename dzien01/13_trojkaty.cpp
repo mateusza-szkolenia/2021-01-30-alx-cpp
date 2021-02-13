83a6afb492f14869cf85166978a85423075d2299
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-13 10:20:24 +0100

    nierówność trójkąta - dozwalam trójkąty o zerowym polu

--- dzien01/13_trojkaty.cpp
+++ dzien01/13_trojkaty.cpp
@@ -5,7 +5,7 @@
 int main() {
     int a, b, c;
     std::cin >> a >> b >> c;
-    if (a + b > c && a + c > b && b + c > a) {
+    if (a + b >= c && a + c >= b && b + c >= a) {
         std::cout << "OK\n";
     }
     else {
