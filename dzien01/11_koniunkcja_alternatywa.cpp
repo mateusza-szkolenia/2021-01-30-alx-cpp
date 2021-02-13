4717cb1b54846d16dbc8706beb616e423e479abe
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-13 10:19:57 +0100

    operacje logiczne - wyniki

--- dzien01/11_koniunkcja_alternatywa.cpp
+++ dzien01/11_koniunkcja_alternatywa.cpp
@@ -3,21 +3,21 @@
 int main() {
     //AND
     std::cout << "koniunkcja:\n";
-    std::cout << (true && true) << '\n';
-    std::cout << (true && false) << '\n';
-    std::cout << (false && true) << '\n';
-    std::cout << (false && false) << '\n';
+    std::cout << (true && true) << '\n';    // true
+    std::cout << (true && false) << '\n';   // false
+    std::cout << (false && true) << '\n';   // false
+    std::cout << (false && false) << '\n';  // false
 
     //OR
     std::cout << "alternatywa:\n";
-    std::cout << (true || true) << '\n';
-    std::cout << (true || false) << '\n';
-    std::cout << (false || true) << '\n';
-    std::cout << (false || false) << '\n';
+    std::cout << (true || true) << '\n';    // true
+    std::cout << (true || false) << '\n';   // true
+    std::cout << (false || true) << '\n';   // true
+    std::cout << (false || false) << '\n';  // false
 
     //NOT
     std::cout << "negacja:\n";
-    std::cout << (!true) << '\n';
-    std::cout << (!false) << '\n';
+    std::cout << (!true) << '\n';           // false
+    std::cout << (!false) << '\n';          // true
     return 0;
 }
