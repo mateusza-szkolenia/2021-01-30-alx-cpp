9d5569ffc4ef08a501dcebe5fad7f349eddd76a1
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-13 14:18:14 +0100

    rysowanie wykresu

--- dzien03/zadanie02.cpp
+++ dzien03/zadanie02.cpp
@@ -2,6 +2,12 @@
 #include <cstdlib>
 #include <ctime>
 
+void narysuj_wykres(int wartosc){
+    for(int i=0; i<wartosc; i++){
+        std::cout << "█";
+    }
+}
+
 int k6(){
     return std::rand() % 6 + 1;
     // zwraca 1, 2, 3, 4, 5 lub 6
@@ -31,8 +37,11 @@ int main(){
     }
 
     for (int i=1; i<=MAKSYMALNY_WYNIK; i++){
-        std::cout << i << ": " << licznik[i] << "\n";
+        std::cout << i << ": ";
+        narysuj_wykres(licznik[i]);
+        std::cout << " " << licznik[i] << "\n";
     }
+
 
     return 0;
 }
