4ffa0a87dbf6e1892c88a8013675d9c8e4864156
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-13 10:20:39 +0100

    inne komentarzw

--- dzien01/08_instrukcje_warunkowe.cpp
+++ dzien01/08_instrukcje_warunkowe.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
 int main() {
-    int a = 55;
+    int a = 5;
     if (a < 10) {
         std::cout << "lalala\n";
     }
     else {
         std::cout << "ffffffffffffffffffff\n";
     }
-
+    std::cout << "ggggg\n";
+    std::cout << "hhhhhh\n";
+    std::cout << "KONIEC\n";
     return 0;
 }
--- dzien01/17_random.cpp
+++ dzien01/17_random.cpp
@@ -5,6 +5,8 @@
 int main() {
     // std::cout << std::time(nullptr) << '\n';
     std::srand(std::time(nullptr));
+    //std::srand(123);
+    //std::cout << std::time(nullptr) << '\n';
     std::cout << std::rand() << '\n';
     std::cout << std::rand() << '\n';
     std::cout << std::rand() << '\n';
