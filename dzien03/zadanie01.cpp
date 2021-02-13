b830eede0e37a42b323b5c699da5b8d55df8afa9
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-13 11:57:59 +0100

    maksimum - nie do końca dobrze

--- dzien03/zadanie01.cpp
+++ dzien03/zadanie01.cpp
@@ -12,20 +12,26 @@ int main()
     int liczby[ILE];
 
     int suma = 0;
+    int maks = 0;
     double srednia;
 
     for(int i = 0; i < ILE; i += 1){
         std::cin >> liczby[i];
     }
 
+
     for(int i=0; i < ILE; i += 1){
         suma += liczby[i];
+        if (liczby[i] > maks){
+            maks = liczby[i];
+        }
     }
 
     srednia = 1.0 * suma / ILE;
 
     std::cout << "Suma: " << suma << "\n";
     std::cout << "Średnia: " << srednia << "\n";
+    std::cout << "Maksimum: " << maks << "\n";
 
     return 0;
 }
