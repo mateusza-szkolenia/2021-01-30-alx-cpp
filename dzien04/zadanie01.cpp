a0e125a8e280684ceb6022fb33b311d17244aed3
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-14 11:33:35 +0100

    funkcja mnoz() itp

--- dzien04/zadanie01.cpp
+++ dzien04/zadanie01.cpp
@@ -10,18 +10,29 @@ void wypisz(Ulamek u){
     std::cout << u.licznik << "/" << u.mianownik << "\n";
 }
 
+Ulamek mnoz(Ulamek u1, Ulamek u2){
+    Ulamek wynik;
+    wynik.licznik = u1.licznik * u2.licznik;
+    wynik.mianownik = u1.mianownik * u2.mianownik;
+    return wynik;
+}
+
 int main()
 {
     Ulamek u1;
     Ulamek u2;
+    Ulamek u3;
 
-    u1.licznik = 1;
+    u1.licznik = 2;
     u1.mianownik = 3;
 
-    u2.licznik = 1;
+    u2.licznik = 3;
     u2.mianownik = 4;
+
+    u3 = mnoz(u1, u2);
 
     wypisz(u1);
     wypisz(u2);
+    wypisz(u3);
 
 }
