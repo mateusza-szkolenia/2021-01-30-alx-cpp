6a76510603792baff790374238356ba131bfe300
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-14 10:34:51 +0100

    zamiana wskaźników na referencje

--- dzien03/zadanie05c.cpp
+++ dzien03/zadanie05c.cpp
@@ -5,40 +5,40 @@ struct Punkt {
     double y;
 };
 
-void inicjuj_punkt(Punkt *p){
-    p->x = (std::rand() % 10000) / 50.0 - 100;
-    p->y = (std::rand() % 10000) / 50.0 - 100;
+void inicjuj_punkt(Punkt &p){
+    p.x = (std::rand() % 10000) / 50.0 - 100;
+    p.y = (std::rand() % 10000) / 50.0 - 100;
 }
 
 void inicjuj_punkty(Punkt *punkty, int ile){
     for (int i=0; i<ile; i++){
-        inicjuj_punkt(&punkty[i]);
+        inicjuj_punkt(punkty[i]);
     }
 }
 
-int sprawdz_os(Punkt *p){
-    if (p->y == 0) return 0; // oś X
-    if (p->x == 0) return 1; // oś Y
+int sprawdz_os(Punkt &p){
+    if (p.y == 0) return 0; // oś X
+    if (p.x == 0) return 1; // oś Y
     return -1;
 }
 
-int sprawdz_cwiartke(Punkt *p){
-    if (p->x > 0 && p->y > 0) return 0; // I ćwiartka
-    if (p->x < 0 && p->y > 0) return 1; // II ćwiartka
-    if (p->x < 0 && p->y < 0) return 2; // III ćwiartka
-    if (p->x > 0 && p->y < 0) return 3; // IV ćwiartka
+int sprawdz_cwiartke(Punkt &p){
+    if (p.x > 0 && p.y > 0) return 0; // I ćwiartka
+    if (p.x < 0 && p.y > 0) return 1; // II ćwiartka
+    if (p.x < 0 && p.y < 0) return 2; // III ćwiartka
+    if (p.x > 0 && p.y < 0) return 3; // IV ćwiartka
     return -1;
 }
 
 void zlicz_punkty_na_oic(Punkt *punkty, int ile, int *osie, int *cwiartki)
 {
     for (int i=0; i<ile; i++){
-        int ktora_os = sprawdz_os(&punkty[i]);
+        int ktora_os = sprawdz_os(punkty[i]);
         if (ktora_os != -1){
             osie[ktora_os]++;
             continue;
         }
-        int ktora_cw = sprawdz_cwiartke(&punkty[i]);
+        int ktora_cw = sprawdz_cwiartke(punkty[i]);
         if (ktora_cw != -1){
             cwiartki[ktora_cw]++;
         }
