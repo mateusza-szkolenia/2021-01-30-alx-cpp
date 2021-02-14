aa05627707a35b4d9d554d24c0a72f28b4102277
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-14 11:39:24 +0100

    dodawanie ulamków

--- dzien04/zadanie01.cpp
+++ dzien04/zadanie01.cpp
@@ -6,13 +6,23 @@ struct Ulamek
     int mianownik;
 };
 
-void wypisz(Ulamek u){
+void wypisz(Ulamek u)
+{
     std::cout << u.licznik << "/" << u.mianownik << "\n";
 }
 
-Ulamek mnoz(Ulamek u1, Ulamek u2){
+Ulamek mnoz(Ulamek u1, Ulamek u2)
+{
     Ulamek wynik;
     wynik.licznik = u1.licznik * u2.licznik;
+    wynik.mianownik = u1.mianownik * u2.mianownik;
+    return wynik;
+}
+
+Ulamek dodaj(Ulamek u1, Ulamek u2)
+{
+    Ulamek wynik;
+    wynik.licznik = u1.licznik * u2.mianownik + u2.licznik * u1.mianownik;
     wynik.mianownik = u1.mianownik * u2.mianownik;
     return wynik;
 }
@@ -22,6 +32,7 @@ int main()
     Ulamek u1;
     Ulamek u2;
     Ulamek u3;
+    Ulamek u4;
 
     u1.licznik = 2;
     u1.mianownik = 3;
@@ -30,9 +41,11 @@ int main()
     u2.mianownik = 4;
 
     u3 = mnoz(u1, u2);
+    u4 = dodaj(u1, u2);
 
     wypisz(u1);
     wypisz(u2);
     wypisz(u3);
+    wypisz(u4);
 
 }
