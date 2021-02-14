d91fa7721b665aed481f32a102ac241f68a30100
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-14 16:41:16 +0100

    grafika NetPBM

---
+++ dzien04/07_grafika_netpbm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int w=64, h=64;
+
+    std::cout << "P3\n";
+    std::cout << w << " " << h << " " << 255 << "\n";
+    for (int y=0; y<h; y++){
+        for(int x=0; x<w; x++){
+            int r, g, b;
+            r = 20; g = 80; b = 10;
+            if (y > w/3){
+                r = 250; g = 250; b = 10;
+            }
+            std::cout << r << " " << g << " " << b << "\n";
+        }
+    }
+
+}
