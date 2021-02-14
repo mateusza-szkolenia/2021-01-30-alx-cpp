6f3a69f527db2545829c1859411b8c4e82de35d2
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-14 15:30:36 +0100

    kopia pliku

---
+++ dzien04/05_przeciazanie_op.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+
+int gcd(const int a, const int b){
+    if (a==0) return b;
+    if (b==0) return a;
+    if (a==b) return a;
+    return (a > b) ? gcd(a%b, b) : gcd(b%a, a);
+}
+
+struct Ulamek
+{
+    int licznik;
+    int mianownik;
+
+    Ulamek() : Ulamek(0, 1){
+    }
+
+    Ulamek(int licznik, int mianownik){
+        // std::cout << "Hej! Jestem nowym Ulamkiem! " << this << " L=" << licznik << " M=" << mianownik << "\n";
+        this->mianownik = mianownik;
+        this->licznik = licznik;
+        this->skroc_sie();
+    }
+
+    void wypisz() const
+    {
+        std::cout << licznik << "/" << mianownik << "\n";
+    }
+    Ulamek mnoz(const Ulamek &u2) const
+    {
+        return Ulamek {
+            licznik * u2.licznik,
+            mianownik * u2.mianownik
+        };
+    }
+
+    Ulamek dziel(const Ulamek &u2) const
+    {
+        return Ulamek {
+            licznik * u2.mianownik,
+            mianownik * u2.licznik
+        };
+    }
+
+    Ulamek dodaj(const Ulamek &u2) const
+    {
+        return Ulamek {
+            licznik * u2.mianownik + u2.licznik * mianownik,
+            mianownik * u2.mianownik
+        };
+    }
+
+    Ulamek odejmij(const Ulamek &u2) const
+    {
+        return Ulamek {
+            licznik * u2.mianownik - u2.licznik * mianownik,
+            mianownik * u2.mianownik
+        };
+    }
+
+    void skroc_sie(){
+        if (mianownik<0){
+            licznik = -licznik;
+            mianownik = -mianownik;
+        }
+        int x = gcd(std::abs(licznik), mianownik);
+        licznik /= x;
+        mianownik /= x;
+    }
+
+};
+
+void napiszUlamek(const Ulamek &u){
+    std::cout << "Ulamek wynosi: ";
+    u.wypisz();
+}
+
+int main()
+{
+    Ulamek u1 = Ulamek(5, 19);
+    Ulamek u2{7, 17};
+    Ulamek u3, u4, u5, u6;
+
+    u3.wypisz();
+    std::cout << "Dotychczas powinno być 6 Ulamków\n";
+
+    // Było tak:
+    /*
+    u3 = mnoz(u1, u2);
+    u4 = dodaj(u1, u2);
+    */
+
+    // Teraz jest jak:
+    u3 = u1.mnoz(u2);
+    u4 = u1.dodaj(u2);
+    u5 = u1.odejmij(u2);
+    u6 = u1.dziel(u2);
+
+    napiszUlamek(u1);
+    napiszUlamek(u2);
+
+    std::cout << "Iloczyn: ";
+    u3.wypisz();
+
+    std::cout << "Suma: ";
+    u4.wypisz();
+
+    std::cout << "Różnica: ";
+    u5.wypisz();
+
+    std::cout << "Iloraz: ";
+    u6.wypisz();
+
+}
