0a29d6d8e434c79cb2209af739a037a9fefcb7b4
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-20 17:05:53 +0100

    rozbicie kodu na implementację i pliki nagłówkowe

---
+++ dzien05/Tablica.hpp
@@ -0,0 +1,24 @@
+class Tablica {
+    public:
+        Tablica(int);
+        Tablica(const Tablica &t);
+        Tablica& operator= (const Tablica &t);
+        Tablica operator+ (Tablica &t);
+        ~Tablica();
+        void ustaw(int pozycja, int wartosc);
+        int daj(int pozycja);
+
+        void wyswietl();
+        void zmien_rozmiar(int n);
+
+        void wypelnij_zakres(int poz_od, int poz_do, int wartosc);
+        void wypelnij(int wartosc);
+
+        void rozszerz(Tablica &t);
+
+    private:
+        int rozmiar;
+        int *dane;
+        bool czy_poprawna_pozycja(int pozycja);
+
+};
---
+++ dzien05/test_tablica.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Tablica.hpp"
+
+int main(){
+    Tablica t1{6};
+    t1.wypelnij(600);
+
+    t1.wyswietl();
+}
