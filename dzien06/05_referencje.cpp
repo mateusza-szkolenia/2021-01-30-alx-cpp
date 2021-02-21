067f5baa6c9373de8c3fa5b237c39ee1e62433c5
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-21 09:53:24 +0100

    przerobienie wskaźników na referencje

--- dzien06/05_referencje.cpp
+++ dzien06/05_referencje.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-void powitanie(std::string imie, int *plicznik){
-    std::cout << "Witaj, " << imie << " (" << *plicznik << ")\n";
-    (*plicznik)++;
+void powitanie(std::string imie, int &licznik){
+    std::cout << "Witaj, " << imie << " (" << licznik << ")\n";
+    licznik++;
 }
 
 int main(){
     int licznik_m=0, licznik_k=0;
 
-    powitanie("Karol", &licznik_m);
-    powitanie("Michal", &licznik_m);
-    powitanie("Piotr", &licznik_m);
-    powitanie("Anna", &licznik_k);
-    powitanie("Mateusz", &licznik_m);
+    powitanie("Karol", licznik_m);
+    powitanie("Michal", licznik_m);
+    powitanie("Piotr", licznik_m);
+    powitanie("Anna", licznik_k);
+    powitanie("Mateusz", licznik_m);
 
     std::cout << "M=" << licznik_m << " K=" << licznik_k << "\n";
 
