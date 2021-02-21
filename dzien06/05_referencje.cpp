2acb1947bf6e2c8e69937e5b05c5298544a6b0eb
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-21 09:50:47 +0100

    kopia

---
+++ dzien06/05_referencje.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void powitanie(std::string imie, int *plicznik){
+    std::cout << "Witaj, " << imie << " (" << *plicznik << ")\n";
+    (*plicznik)++;
+}
+
+int main(){
+    int licznik_m=0, licznik_k=0;
+
+    powitanie("Karol", &licznik_m);
+    powitanie("Michal", &licznik_m);
+    powitanie("Piotr", &licznik_m);
+    powitanie("Anna", &licznik_k);
+    powitanie("Mateusz", &licznik_m);
+
+    std::cout << "M=" << licznik_m << " K=" << licznik_k << "\n";
+
+}
