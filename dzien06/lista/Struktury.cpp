d0389ae6214fdf99a4835724430c4d1430ab355f
Mateusz Adamowski <m.adamowski@alx.pl>
2021-02-21 15:57:37 +0100

    zerowanie wskaźników

--- dzien06/lista/Struktury.cpp
+++ dzien06/lista/Struktury.cpp
@@ -3,11 +3,14 @@
 #include "Struktury.hpp"
 
 Lista::Lista():
-    licznik(0)
+    licznik(0),
+    pierwszy(nullptr),
+    ostatni(nullptr)
 {
 }
 
 void Lista::pokaz(){
     std::cout << "--[ LISTA ("<< licznik <<") ]--\n";
+    std::cout << "<" << this->pierwszy << " .. " << this->ostatni << ">\n";
     std::cout << "------------------\n";
 }
