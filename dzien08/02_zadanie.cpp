fd01852a973b1dfdc42bef1f0a7a108f4e691409
Mateusz Adamowski <m.adamowski@alx.pl>
2021-03-07 11:49:30 +0100

    implementacja

--- dzien08/02_zadanie.cpp
+++ dzien08/02_zadanie.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+
 /*
 Zadanie: stworzyć szablon funkcji przeliczającej temperaturę z C na F
 */
+
+template <typename T>
+T c2f(T temp_c){
+    return temp_c * 1.8 + 32;
+}
+
+int main(){
+    std::cout << c2f(20) << "\n";
+    std::cout << c2f(36.6) << "\n";
+}
